0859a578d8cbe2fe3acd2fbe4c46c6c0e67e33c3
Carl Anderson <>
2018-12-10 15:16:43 +0000

    Updates

---
+++ precache-manifest.d789287b9122a543edb8105155de4d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c39bb50f22fefcb531",
+    "url": "/react-chess/static/js/runtime~main.65c39bb5.js"
+  },
+  {
+    "revision": "903953e9c22cb853eae4",
+    "url": "/react-chess/static/js/main.903953e9.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/react-chess/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "903953e9c22cb853eae4",
+    "url": "/react-chess/static/css/main.b17c9948.chunk.css"
+  },
+  {
+    "revision": "c6c4f138dde61c0d924067da7124b9f7",
+    "url": "/react-chess/index.html"
+  }
+];
